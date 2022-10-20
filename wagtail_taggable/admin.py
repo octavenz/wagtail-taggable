d881beca0a2ae8d901bb1394419dc7ea0411f793
Richard Campen <richard.campen@octave.nz>
2022-10-20 17:19:52 +1300

    feat: add admin styles and inject using Media on the TagsPanel

--- wagtail_taggable/admin.py
+++ wagtail_taggable/admin.py
@@ -5,8 +5,14 @@ from wagtail.admin.panels import InlinePanel
 
 class TagsPanel(InlinePanel):
     """Custom Inline Panel variant with simplified UI for rendering lists of tags."""
+
     class BoundPanel(InlinePanel.BoundPanel):
         template_name = 'tags/tags_panel.html'
+
+        class Media:
+            css = {
+                'all': ('tags/css/tag-panel.css',)
+            }
 
 
 class TagChooser(AdminChooser):
