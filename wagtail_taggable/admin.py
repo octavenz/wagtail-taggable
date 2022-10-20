75ccca6a56689323b28b4778b8a05a1352c0a2c1
Richard Campen <richard.campen@octave.nz>
2022-10-21 09:28:55 +1300

    chore: undo the nested styling applied by the parent InlinePanel class.

--- wagtail_taggable/admin.py
+++ wagtail_taggable/admin.py
@@ -6,6 +6,10 @@ from wagtail.admin.panels import InlinePanel
 class TagsPanel(InlinePanel):
     """Custom Inline Panel variant with simplified UI for rendering lists of tags."""
 
+    def classes(self):
+        # InlinePanel renders with the nested panel interface but we don't need that here.
+        return [class_ for class_ in super().classes() if class_ != 'w-panel--nested']
+
     class BoundPanel(InlinePanel.BoundPanel):
         template_name = 'wagtail_taggable/tags_panel.html'
 
