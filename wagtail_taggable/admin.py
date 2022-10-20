0d48798bde99e78627405bb01cbd223429c2a460
Richard Campen <richard.campen@octave.nz>
2022-10-20 17:22:10 +1300

    fix: correct template directory name to reflect latest django app name

--- wagtail_taggable/admin.py
+++ wagtail_taggable/admin.py
@@ -7,13 +7,13 @@ class TagsPanel(InlinePanel):
     """Custom Inline Panel variant with simplified UI for rendering lists of tags."""
 
     class BoundPanel(InlinePanel.BoundPanel):
-        template_name = 'tags/tags_panel.html'
+        template_name = 'wagtail_taggable/tags_panel.html'
 
         class Media:
             css = {
-                'all': ('tags/css/tag-panel.css',)
+                'all': ('wagtail_taggable/css/tag-panel.css',)
             }
 
 
 class TagChooser(AdminChooser):
-    template = 'tags/tag_chooser.html'
+    template = 'wagtail_taggable/tag_chooser.html'
